1861ad376a38831282755bfcbc22f1033f55d636
Sanket Mohapatra <sanketmohapatra13@gmail.com>
2025-03-29 21:05:02 +0530

    fix(package-manager): Switch to UV

---
+++ README.md
@@ -0,0 +1,10 @@
+# TPCODL Scrapper
+Tired with the power cuts in your area (Yes, I am!), then a tool built for me and people like me..
+
+## Capability 
+It scraps the TPCODL website for the power cuts in my area and display it in the form of a JSON array. Thats it.
+
+### How to run?
+1. Make a new venv `uv venv --python 3.12` and activate it. 
+2. Install the deps `uv sync`.
+3. Just run the `main.py` python file.
--- src/script.py
+++ src/script.py
@@ -8,7 +8,7 @@ import json
 
 def scrap():
     # URL for the TPCODL Website
-    url = 'https://kavach.tpodisha.com/(S(k5jbomhuadjjihxbbzfl4lqj))/Reports/PSDCases?discom=CODL'
+    url = "https://kavach.tpodisha.com/(S(k5jbomhuadjjihxbbzfl4lqj))/Reports/PSDCases?discom=CODL"
 
     # %%
     # Fetch the data from the website
@@ -19,19 +19,18 @@ def scrap():
 
     # %%
 
-    soup = BeautifulSoup(page.text, 'html.parser')
+    soup = BeautifulSoup(page.text, "html.parser")
 
     # %%
     # Target the Suraksha Kavach Table
-    suraksha_table = soup.find(
-        'table', {'id': "MainContent_GV_SurakshaKavach"})
+    suraksha_table = soup.find("table", {"id": "MainContent_GV_SurakshaKavach"})
 
     # %%
     # Check the table content
     if suraksha_table is None:
         pass
     else:
-        table_rows = suraksha_table.find_all('tr')  # type: ignore
+        table_rows = suraksha_table.find_all("tr")  # type: ignore
 
         # Each td has a format of id="MainContent_GV_SurakshaKavach_div2_<row number>"
 
@@ -42,7 +41,7 @@ def scrap():
             # Get the heading separately in a array
 
             # find all the th
-            headings = tr.findAll('th', {"class": "text-nowrap"})
+            headings = tr.findAll("th", {"class": "text-nowrap"})
             if headings is not None:
                 for heading in headings:
                     heading_block.append(heading.text)
@@ -51,17 +50,20 @@ def scrap():
             # print(td)
             td_division_field = tr.find("div")
 
-            if td_division_field is not None and "div3" in td_division_field['id']:
+            if td_division_field is not None and "div3" in td_division_field["id"]:
                 td_gss_field = td_division_field.find_next("div")
-                if "BCDD-II, BBSR" in td_division_field.text and "CS PUR-2" in td_gss_field.text:
+                if (
+                    "BCDD-II, BBSR" in td_division_field.text
+                    and "CS PUR-2" in td_gss_field.text
+                ):
                     # Get all the field for the TR in the JSON format
-                    all_div = tr.findAll('div')
+                    all_div = tr.findAll("div")
                     temp_block = {}
                     for idx, div in enumerate(all_div):
-                        temp_block[heading_block[idx+2]] = div.text
+                        temp_block[heading_block[idx + 2]] = div.text
                     final_block.append(temp_block)
 
-        with open('sample_data.json', 'w', encoding='utf-8') as f:
+        with open("sample_data.json", "w", encoding="utf-8") as f:
             json.dump(final_block, f, ensure_ascii=False, indent=4)
 
-    print('Operation Complete!')
+    print("Operation Complete!")
